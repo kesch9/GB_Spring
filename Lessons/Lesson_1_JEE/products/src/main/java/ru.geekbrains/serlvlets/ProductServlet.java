3b70d584b19f381c1dd20e3952578f25492872b5
KashenkovSergej <kashenkovsergej@yandex.ru>
2021-02-10 20:43:28 +0300

    Homework1

--- Lessons/Lesson_1_JEE/products/src/main/java/ru.geekbrains/serlvlets/ProductServlet.java
+++ Lessons/Lesson_1_JEE/products/src/main/java/ru.geekbrains/serlvlets/ProductServlet.java
@@ -1,4 +1,4 @@
-package ru.geekbrains.servlet;
+package ru.geekbrains.serlvlets;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,20 +14,30 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@WebServlet(name = "ProductServlet", urlPatterns = "/products")
+@WebServlet(name = "ProductServlet", urlPatterns = "/all")
 public class ProductServlet extends HttpServlet {
 
-    private static Logger logger = LoggerFactory.getLogger(BasicServlet.class);
+    private static Logger logger = LoggerFactory.getLogger(ProductServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         logger.debug("Start");
+        int size = DEFAULT;
+        try{
+            size = Integer.parseInt(req.getParameter("size"));
+        } catch (Exception e){
+            logger.debug(e.getMessage());
+        }
         Stream<Integer> infiniteStream = Stream.iterate(0, i -> ++i);
         List<Product> products = infiniteStream
-                .limit(10)
+                .limit(size)
                 .map(x -> new Product(x, Integer.toString(x), x))
                 .collect(Collectors.toList());
-        resp.getWriter().println("<p> " + products.get(0) + "</p>");
+        for(Product product : products) {
+            resp.getWriter().println("<p> " + product + "</p>");
+        }
         resp.getWriter().close();
     }
+
+    private static int DEFAULT = 10;
 }
